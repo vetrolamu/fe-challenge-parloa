923e2252b5d294d1974419fde5fdff171f6ad107
Alexey Gerasimenko <agerasimenko@wayfair.com>
2025-05-21 23:50:42 +0200

    [PRL-0] Fix eslint / prettier configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import testingLibrary from 'eslint-plugin-testing-library'
 import tseslint from 'typescript-eslint'
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -18,9 +21,11 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
+      react: react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      'testing-library': testingLibrary
+      'testing-library': testingLibrary,
+      prettier: prettierPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -28,6 +33,8 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'prettier/prettier': 'error',
     },
   },
+  prettierConfig
 )
